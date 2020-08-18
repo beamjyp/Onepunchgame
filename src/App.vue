92a207f0fb6a791c7dff99386cc3bb400ef20090
beamjyp <68525436+beamjyp@users.noreply.github.com>
2020-08-18 13:05:29 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.jpg" />
+    <HelloWorld msg="Welcome to The one punch man game"/>
+    <div class="row">
+      <div class="col"><HelloWorld msg="select the hero"/></div>
+      </div>
+      <div class="col"><Heropic/></div>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Heropic from './components/Heropic.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Heropic,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Heropic.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <div><button @click="randomhero(0,3)" class="btn btn-info">กดเพื่อเป็น Hero</button>
+        <h1>{{player.name}} {{monster}}</h1></div>
+    <div class="col"><button @click="randomDamage(5,10)" class="btn btn-info">กดเพื่อโจมตีธรรมดา</button>
+    <button @click="randomDamage(7,15)" class="btn btn-info">กดเพื่อโจมตีท่าไม้ตาย</button></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+        name:'',
+        hp:0,
+      player:[
+          {name:'saitama', hp:9999,image:"./assets/h1.jpg"},
+          {name:'tasumaki', hp:200,image:'h2.jpg'},
+          {name:'janos', hp:125,image:'h3.jpg'},
+          {name:'mumen rider', hp:35,image:'h4.jpg'},
+      ],
+      monster:[
+          {name:'Giantfukakao', hp:120,image:'m1.jpg'},
+          {name:'Kingofdeepsea', hp:100,image:'m2.jpg'},
+          {name:'Carpkung', hp:35,image:'m3.jpg'},
+          {name:'Boros', hp:200,image:'m4.jpg'}
+      ],
+      
+    };
+  },
+  methods: {
+    randomhero: function (min, max) {
+      this.player = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.monster = Math.max(Math.floor(Math.random() * max) + 1, min);
+    },
+    randomDamage: function (min, max) {
+      this.player = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.monster = Math.max(Math.floor(Math.random() * max) + 1, min);
+      this.player-this.monster.hp
+      this.monster-this.player.hp
+  },
+  
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
